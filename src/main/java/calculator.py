25ccec0a84500d58279201213cf02ec6d99a088d
oksanaShafransky <41061297+oksanaShafransky@users.noreply.github.com>
2018-11-04 13:53:05 +0200

    Create calculator.py

---
+++ src/main/java/calculator.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  4 12:03:39 2018
+
+@author: oksanas
+"""
+
+#simple_calculator
+
+def add(x,y):
+    return x+y
+
+def subtract(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    return (x / y)
+
+import sys
+
+while 1:
+    print("Select operation \n1) add \n2) subtract \n3) multipfy \n4) divide \n5) exit")
+    choice = int(input())
+    if choice == 5:
+        sys.exit("bye bye")
+    elif choice >=1 and choice <=4:    
+        num1 = int(input("Input first number: "))
+        num2 = int(input("Input second number: "))
+
+    if choice == 1:
+        print(num1,"+",num2,"=",add(num1,num2))
+    elif choice == 2:
+        print(num1,"-",num2,"=",subtract(num1,num2))
+    elif choice == 3:
+        print(num1,"*",num2,"=",multiply(num1,num2))
+    elif choice == 4:
+        print(num1,"/",num2,"=",divide(num1,num2))
+    else:
+        print("Invalid input")
+    
